8d10d8768a86405dbd1a62621f1d095e93cf1523
Santiago Enol <santieherrero@gmail.com>
2015-07-23 16:37:27 +0200

    Add header response to Allow Origin

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-var express, app;
+var express, app, emailer;
 
 express = require('express');
 app = express();
+emailer = require('./controllers/emailer');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/',function(request,response) {
   response.send('OK');
 });
 
+
+app.post('/mailer',emailer.check,emailer.sendEmail);
 
 module.exports = app;
